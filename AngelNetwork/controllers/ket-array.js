8419e8a07232b2f1e718150b9545c84ddb3efd49
wjcking <easyfound@126.com>
2021-01-03 16:32:18 +0800

    aop-

--- AngelNetwork/controllers/ket-array.js
+++ AngelNetwork/controllers/ket-array.js
@@ -3,5 +3,5 @@ String.prototype.array = function (s) {
   'use strict';
     
  var number = 0;
- 
+    
 }
--- AngelNetwork/models/ket-utility.js
+++ AngelNetwork/models/ket-utility.js
@@ -1,4 +1,20 @@
 ﻿"use strict";
+const separator_space = ' ';
+const separator_comma = ',';
+const separator_quest = "?.";
+const separator_pointer = "->";
+//1 balance parallel to artlisense 平衡 美 图
+//2 Atowards:color.impluse.first
+//3 Ageing:infancy~endportal:1.3.8.16.25.30.35.45.50.55.60.70.80.100.110.120.?.220
+//4 disabled 
+//5 1 运动类：篮球、羽毛球、兵乓球、足球、滑板、滑旱冰、跑步、跳绳、举重……（兴趣/爱好）
+//6 武术：跆拳道、自由搏击艺术：涂鸦、插花、看展览、听相声游戏：桌游、网游、手游
+Kits = [
+    [separator_space, separator_comma, separator_quest, separator_pointer]
+]
+Kit Line Index
+    separator_space
+    [0,1,7,7],[1,9,16,7]
 Cybers = [
     ['Separator'],
     ['Link'],
