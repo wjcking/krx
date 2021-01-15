db9eba2e720e1546917bfca9ff24813b017bb603
wjcking <wjcking@hotmail.com>
2021-01-15 21:22:38 +0800

    fos eof

---
+++ AngelNetwork/models/leon-caret.js
@@ -0,0 +1,12 @@
+﻿Intelligent = ["Human", "Rockmega", "Leon"];
+Leon = ["Source", "Orgin"]; 
+Maticals = ["Flat", "Tricycle", "Metric"];
+Axis = ["Center","Side", "Angle"]
+Solid = ["Circle", "Trianet", "Frant"] //Radical Rectant
+//
+//Rectan.Circle.Fracten
+//Facten.Circle.Rectan
+//Flex^I^able
+//Frant.Fectan
+//Frant.Rectan
+Anglo = ["Triangle", "Rectangle"]// Iregular Regular//Circulet Sequallet Sequalled 
