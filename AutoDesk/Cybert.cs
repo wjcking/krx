ce67b80923bd27ca18cbd4cb48c38363ab23fb90
wjcking <wjcking@hotmail.com>
2021-01-17 07:00:08 +0800

    8 mark

--- AutoDesk/Cybert.cs
+++ AutoDesk/Cybert.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace AutoDesk
 {
     /// <summary>
@@ -7,16 +7,18 @@ namespace AutoDesk
     /// 编译器的基础
     ///     Cybers:Separator Connector Bracket
     ///     sot cok bot
+    ///    generic.programming.algorithm par war
     /// </summary>
     public class Cybert : Hypert
     {
         public Cybert(object value)
         {
-
+          List<string>
         }
         public Cybert(string value)
         {
 
         }
+        
     }
 }
