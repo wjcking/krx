2ae5d0909fa5f4972f9b11889db2c936bb0060ec
wjcking <easyfound@126.com>
2021-01-19 09:22:06 +0800

    listbox

--- AngelLayout/Midow.Designer.cs
+++ AngelLayout/Midow.Designer.cs
@@ -62,6 +62,7 @@
             // 
             // menuStrip
             // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.MenuMain,
             this.检索SToolStripMenuItem,
@@ -70,7 +71,8 @@
             this.帮助HToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(790, 25);
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip.Size = new System.Drawing.Size(1580, 43);
             this.menuStrip.TabIndex = 37;
             this.menuStrip.Text = "menuStrip";
             // 
@@ -82,31 +84,31 @@
             this.toolStripMenuItem2,
             this.menuQuit});
             this.MenuMain.Name = "MenuMain";
-            this.MenuMain.Size = new System.Drawing.Size(58, 21);
+            this.MenuMain.Size = new System.Drawing.Size(103, 35);
             this.MenuMain.Text = "文件(&F)";
             // 
             // tsm_new
             // 
             this.tsm_new.Name = "tsm_new";
-            this.tsm_new.Size = new System.Drawing.Size(139, 22);
+            this.tsm_new.Size = new System.Drawing.Size(237, 38);
             this.tsm_new.Text = "新建(&N)";
             // 
             // menuMyFile
             // 
             this.menuMyFile.Name = "menuMyFile";
             this.menuMyFile.ShortcutKeys = System.Windows.Forms.Keys.F4;
-            this.menuMyFile.Size = new System.Drawing.Size(139, 22);
+            this.menuMyFile.Size = new System.Drawing.Size(237, 38);
             this.menuMyFile.Text = "打开(&O)";
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(136, 6);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(234, 6);
             // 
             // menuQuit
             // 
             this.menuQuit.Name = "menuQuit";
-            this.menuQuit.Size = new System.Drawing.Size(139, 22);
+            this.menuQuit.Size = new System.Drawing.Size(237, 38);
             this.menuQuit.Text = "退出(&Q)";
             // 
             // 检索SToolStripMenuItem
@@ -115,20 +117,20 @@
             this.menuSearch,
             this.menuHistory});
             this.检索SToolStripMenuItem.Name = "检索SToolStripMenuItem";
-            this.检索SToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.检索SToolStripMenuItem.Size = new System.Drawing.Size(104, 35);
             this.检索SToolStripMenuItem.Text = "检索(&S)";
             // 
             // menuSearch
             // 
             this.menuSearch.Name = "menuSearch";
             this.menuSearch.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.menuSearch.Size = new System.Drawing.Size(148, 22);
+            this.menuSearch.Size = new System.Drawing.Size(256, 38);
             this.menuSearch.Text = "检索";
             // 
             // menuHistory
             // 
             this.menuHistory.Name = "menuHistory";
-            this.menuHistory.Size = new System.Drawing.Size(148, 22);
+            this.menuHistory.Size = new System.Drawing.Size(256, 38);
             this.menuHistory.Text = "检索历史记录";
             // 
             // 试图VToolStripMenuItem
@@ -136,14 +138,14 @@
             this.试图VToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuStartPage});
             this.试图VToolStripMenuItem.Name = "试图VToolStripMenuItem";
-            this.试图VToolStripMenuItem.Size = new System.Drawing.Size(60, 21);
+            this.试图VToolStripMenuItem.Size = new System.Drawing.Size(106, 35);
             this.试图VToolStripMenuItem.Text = "视图(&V)";
             // 
             // menuStartPage
             // 
             this.menuStartPage.Name = "menuStartPage";
             this.menuStartPage.ShortcutKeys = System.Windows.Forms.Keys.F1;
-            this.menuStartPage.Size = new System.Drawing.Size(133, 22);
+            this.menuStartPage.Size = new System.Drawing.Size(225, 38);
             this.menuStartPage.Text = "起始页";
             // 
             // 工具TToolStripMenuItem
@@ -153,25 +155,25 @@
             this.menuBackup,
             this.menuRestore});
             this.工具TToolStripMenuItem.Name = "工具TToolStripMenuItem";
-            this.工具TToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.工具TToolStripMenuItem.Size = new System.Drawing.Size(104, 35);
             this.工具TToolStripMenuItem.Text = "工具(&T)";
             // 
             // menuSettings
             // 
             this.menuSettings.Name = "menuSettings";
-            this.menuSettings.Size = new System.Drawing.Size(118, 22);
+            this.menuSettings.Size = new System.Drawing.Size(196, 38);
             this.menuSettings.Text = "配置(&O)";
             // 
             // menuBackup
             // 
             this.menuBackup.Name = "menuBackup";
-            this.menuBackup.Size = new System.Drawing.Size(118, 22);
+            this.menuBackup.Size = new System.Drawing.Size(196, 38);
             this.menuBackup.Text = "备份(&B)";
             // 
             // menuRestore
             // 
             this.menuRestore.Name = "menuRestore";
-            this.menuRestore.Size = new System.Drawing.Size(118, 22);
+            this.menuRestore.Size = new System.Drawing.Size(196, 38);
             this.menuRestore.Text = "还原";
             // 
             // 帮助HToolStripMenuItem
@@ -181,41 +183,45 @@
             this.toolStripMenuItem1,
             this.menuRegisterInfo});
             this.帮助HToolStripMenuItem.Name = "帮助HToolStripMenuItem";
-            this.帮助HToolStripMenuItem.Size = new System.Drawing.Size(61, 21);
+            this.帮助HToolStripMenuItem.Size = new System.Drawing.Size(109, 35);
             this.帮助HToolStripMenuItem.Text = "帮助(&H)";
             // 
             // menuHelp
             // 
             this.menuHelp.Name = "menuHelp";
-            this.menuHelp.Size = new System.Drawing.Size(141, 22);
+            this.menuHelp.Size = new System.Drawing.Size(243, 38);
             this.menuHelp.Text = "查看帮助(&H)";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(138, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(240, 6);
             // 
             // menuRegisterInfo
             // 
             this.menuRegisterInfo.Name = "menuRegisterInfo";
-            this.menuRegisterInfo.Size = new System.Drawing.Size(141, 22);
+            this.menuRegisterInfo.Size = new System.Drawing.Size(243, 38);
             this.menuRegisterInfo.Text = "关于(&A)";
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 534);
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1090);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(790, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1580, 22);
             this.statusStrip1.TabIndex = 39;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 43);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(790, 25);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1580, 25);
             this.toolStrip1.TabIndex = 40;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -236,19 +242,21 @@
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(0, 50);
+            this.textBox1.Location = new System.Drawing.Point(0, 68);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(790, 484);
+            this.textBox1.Size = new System.Drawing.Size(1580, 1022);
             this.textBox1.TabIndex = 41;
             this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(651, 130);
+            this.textBox2.Location = new System.Drawing.Point(1302, 260);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(109, 21);
+            this.textBox2.Size = new System.Drawing.Size(214, 35);
             this.textBox2.TabIndex = 45;
             // 
             // label3
@@ -257,9 +265,10 @@
             this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.Cornsilk;
-            this.label3.Location = new System.Drawing.Point(651, 97);
+            this.label3.Location = new System.Drawing.Point(1302, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.Size = new System.Drawing.Size(224, 34);
             this.label3.TabIndex = 44;
             this.label3.Text = "R2.ColorRibbon";
             // 
@@ -269,9 +278,10 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.Cornsilk;
-            this.label1.Location = new System.Drawing.Point(651, 66);
+            this.label1.Location = new System.Drawing.Point(1302, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.Size = new System.Drawing.Size(220, 36);
             this.label1.TabIndex = 42;
             this.label1.Text = "R1.ColorRibbon";
             // 
@@ -282,19 +292,20 @@
             this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label2.Font = new System.Drawing.Font("黑体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(295, 167);
+            this.label2.Location = new System.Drawing.Point(590, 334);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(468, 367);
+            this.label2.Size = new System.Drawing.Size(934, 732);
             this.label2.TabIndex = 43;
-            this.label2.Text = "R1.ColorRibbon";
+            this.label2.Text = "https://www.it610.com/article/4690776.htm";
             this.label2.Paint += new System.Windows.Forms.PaintEventHandler(this.label2_Paint);
             this.label2.Validating += new System.ComponentModel.CancelEventHandler(this.label2_Validating);
             // 
             // Midow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 556);
+            this.ClientSize = new System.Drawing.Size(1580, 1112);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -304,6 +315,7 @@
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Midow";
             this.Text = "[Asnick]wjcking";
             this.menuStrip.ResumeLayout(false);
