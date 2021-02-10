5b08dc495fd6fb67e7b6b9686151e0619362b8f4
wjcking <easyfound@126.com>
2021-02-10 16:08:31 +0800

    dct.kbdtouchpad,virus,

--- AutoDesk/Program.cs
+++ AutoDesk/Program.cs
@@ -1,7 +1,11 @@
-﻿
+﻿#define fok
+
+#define oneAtwoAthrAfivAsixAhubBasfDasoneAtwoAthrAfivAsixAhubBasfDasoneAtwoAthrAfivAsixAhubBasfDasoneAtwoAthrAfivAsixAhubBasfDas
 using Microsoft.CSharp;
 using System;
 using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace AutoDesk
 {
@@ -13,7 +17,7 @@ namespace AutoDesk
             hyb a = new hyb();
             a.ket = '+';
             Console.ForegroundColor = ConsoleColor.Yellow;
- 
+
             atb.func();
             Console.Read();
             /*
@@ -22,13 +26,81 @@ namespace AutoDesk
         }
     }
 }
+    [Description("value only")]
+enum Lser
+{
+    //ab,cd,ef,gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+    //ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+    //ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+    //ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+    //ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+    //ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, ab, cd, ef, gh, asz0
+}
+interface inf
+{
+    void method();
+}
 public struct atb
 {
 
     [System.Diagnostics.Conditional("fok")]
     public static void func()
     {
-        Console.Write("defined fok");
-        Console.Write("defined fok");
+        object condet = null;
+
+        object variant = null;
+        object range = null;
+        object opera = null;
+
+        do
+        {
+        }
+        while (condet != null);
+
+        //for (
+        //    variant;
+        //    range;
+        //    opera;
+        //    )
+        //{
+
+        //}
+        while (condet != null)
+        {
+
+        }
+ /*
+#region
+#define      
+#undef       
+#if              
+#else	      
+#elif	          
+#endif	      
+#line // 它可以让您修改编译器的行数以及（可选地）输出错误和警告的文件名。
+#error	       
+#warning	  
+	   
+#endregion	*/
+//它标识着 #region 块的结束。
+        //Stopwatch sw = new Stopwatch();
+        ////开始计时
+        //sw.Start();
+        ////重新设置为零
+        //sw.Reset();
+        ////重新设置并开始计时
+        //sw.Restart();
+        ////结束计时
+        //sw.Stop();
+        ////获取运行时间间隔
+        //TimeSpan ts = sw.Elapsed;
+        ////获取运行时间[毫秒]
+        //long times = sw.ElapsedMilliseconds;
+        ////获取运行的总时间
+        //long times2 = sw.ElapsedTicks;
+        ////判断计时是否正在进行[true为计时]
+        //bool isrun = sw.IsRunning;
+        ////获取计时频率
+        //long frequency = Stopwatch.Frequency;
     }
 }
