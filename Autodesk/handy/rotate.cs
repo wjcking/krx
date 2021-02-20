064b5a7e77200813da61b81de4b581da500bf7ae
wjcking <easyfound@126.com>
2021-02-20 19:35:44 +0800

    term

---
+++ Autodesk/handy/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+struct rotate
+{
+   
+    static rotate()
+    {
+    
+    }
+   
+   public static void  vio(delgot<bool> kondet, delgov<string,string,int> kondetBody)
+   {  
+       while(kondet())
+       {
+           
+       }
+   }
+}
