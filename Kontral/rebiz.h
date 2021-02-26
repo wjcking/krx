4e0489de636b7d0960a1513a951834c0c5b14e21
wjcking <easyfound@126.com>
2021-02-26 16:10:47 +0800

    define organized

--- Kontral/rebiz.h
+++ Kontral/rebiz.h
@@ -11,3 +11,23 @@ ls1zl, ls2zl,    ls3fzl, ls4zl, ls5zl, ls6zl, ls7zl, ls8zl, ls9ezl,  ls10zl, ls1
 short
 ls53c2el, ls8003el,    ls606fel, ls7f5112el, ls67085el, ls65e5el, ls62a5el, ls9053el, ls636eel,  ls8054el, ls793eel, ls7f8eel,
 ls1el, ls2el,    ls3fel, ls4el, ls5el, ls6el, ls7el, ls8el, ls9eel,  ls10el, ls11eel, ls12el;
+
+/*
+int[] each
+int[][]\r\n
+duanduanjinian，you MIT 
+短短几年，由 MIT CSAIL 实验室开发的编程语言Julia已然成为编程界的新宠，尤其在科学计算领域炙手可热。据介绍，这门语言结合了 C 语言的速度、
+	Ruby 的灵活、Python 的通用性，以及其他各种语言的优势于一身。
+自去年 8 月推出 1.0 版本以来，Julia 上升势头强劲，它的 GitHub Star 数在过去一年翻了一番，
+还被 1000 多种学术出版物引用。根据 2019 年 8 月的 TIOBE 编程语言指数，Julia 已经从 7 月的第 50 名升至第 39 名，在众多语言中上升幅度显著。
+有人将 Julia 称作“未来的编程语言”，有人认为它是 Python 强有力的挑战者，
+开始
+	但也有人吐槽 Julia 的语法简直反人性。
+	你愿意学习 Julia 吗？或者你正在使用 Julia 吗？在你看来，J
+	ulia 有哪些特性特别优秀或者让你特别想吐槽的吗？你认为它可能在未来超越 Python 吗？
+	欢迎参与这个话题讨论，在下方评论区表达你的观点。
+结束
+1 2 3 4 5 6
+
+Mecedes
+*/
