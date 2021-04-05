481674504bb470d3ac93a49382db4241c4a25edd
wjcking <easyfound@126.com>
2021-04-05 10:36:06 +0800

    @结构设计 EryicStruct //eng chs turk

--- Kontral/rebiz.h
+++ Kontral/rebiz.h
@@ -2,14 +2,26 @@
 #include <string>
 using namespace std;
 /*
-English
-	The 4th of April this year marks the Qingming Festival, an important day on the lunar and pay tributes to their ancestors. 
-中文
-	由实验室开发的编程语言已然成为编程界的新宠，尤其在科学计算领域炙手可热。据介绍，这门语言结合了语言的速度、
-Pocca
-	В институте построены учебные корпуса，в которых помешаются аудитории，лекционные залы и кабинеты. 
-Turkish
-	/ شى جىنپىڭ مۇنۇلارنى تەكىتلىدى: جۇڭگو بىلەن پاكىستان پۈتۈن شارائىتلىق ئىستراتېگىيەلىك ھەمكارلىق ھەمراھى. جۇڭگو ھۆكۈمىتى ۋە خەلقى باشتىن - ئاخىر /
+Earth.Human.Language.CivlizeSuperior //ionic-fond.html 56s
+	Ancient.Oracle
+	Original Language Exdistintion
+Eyric Struct
+	Each Batch BoardSpace WordKind
+	Object Variant
+Karact.Keyboard.Symbol.Express
+pfx sfx
+	//~!@#$%^&*()
+	dot: . 0x
+	ace:   
+National Populate
+		English
+			The 4th of April this year marks the Qingming Festival, an important day on the lunar and pay tributes to their ancestors. 
+		中文
+			由实验室开发的编程语言已然成为编程界的新宠，尤其在科学计算领域炙手可热。据介绍，这门语言结合了语言的速度、
+		Pocca
+			В институте построены учебные корпуса，в которых помешаются аудитории，лекционные залы и кабинеты. 
+		Turkish
+			/ شى جىنپىڭ مۇنۇلارنى تەكىتلىدى: جۇڭگو بىلەن پاكىستان پۈتۈن شارائىتلىق ئىستراتېگىيەلىك ھەمكارلىق ھەمراھى. جۇڭگو ھۆكۈمىتى ۋە خەلقى باشتىن - ئاخىر /
 Translect.VoiceDirect.Interpreter
 	English.KeyBoard.Requires.Indentical
 	Transturk.Fundametal
@@ -26,6 +38,71 @@ Translect.VoiceDirect.Interpreter
 VariantName.Must.EnglishKaract
 SpecialKaract
 	\r \n space
+@结构设计 EryicStruct //eng chs turk
+	@钫格 //EricPixel 
+			Dwf.Search 
+			LinqBridge.InstantSearch
+	@单变量 放入 钫格
+		英文字母 char.foreach
+		英文单词 space.word.foreach
+		波浪字母 pasted.char.foreach
+		波浪单词	space.word.foreach
+	@词组  放入 钫格
+		中文拼音每个汉字 //平杨弯降
+			 wo ai gang dan li xian ji 
+		中文拼音词组 //结巴
+			wo ai gangdan lixianji			
+		中文每个汉字拼音
+			each.spaced:w o a i g a n g d a n l i x i a n j i //wo ai gang dan li xian ji 
+		中文每个词组拼音 //汉语词性
+		  jieba: wo ai gangdan lixianji
+	@索引 存放 控制 钫格
+		文件名字 用法
+		存放 纯钫格
+		最底层 
+			十进制 十六进制
+		颜色
+			RGB ARGB
+		符号 
+			五字码 sabce abmcd oabcd
+			a b c yi er san
+		声明关键字和纯钫格
+			*字符 关键字 转换 重复标注
+			用户可以看懂
+			Each letters:
+				a b c d 
+				e f g h
+			Each word:
+				my name is jacky kma_asc_83
+				your age is thrteen dot_asc121
+			标题：早发白帝城
+				朝 辞 白 帝 彩 云 间 照此白底
+				zhao ci bai di cai yun jian zhao1 ci1 bai1 di1 
+				zcbdcyj //ime
+			研发需要转换格式
+			string
+				a, b, c,d
+				e, f, g ,h
+			short
+				na , nb, nc,nd 
+				na , nb, nc,nd 
+			string[] 
+				a,b,c,d 
+			short[] 
+				na,nb,nc,nd 	
+	@钫格 32767 ScreenPixel Width Height
+		动态手持视觉水平像素
+		固定像素宽高
+		索莱得 钫法 地球数学 //Artfod
+		算法 控制 排序 索引 结构 放入 存放 取出	
+		文章 统计 绘画 艺术 哲学
+	@艾昂尼克和词性分类与存放			
+		动 铯  //结构 存放 形状
+		动	铯.形容 不 名 //Renliy
+			象形 拟声 以此类推
+	@混编 
+		例矩：April Festival,由实验室开发的 В институте построены
+		编码算法统计 当场记忆
 Haibit.Andyuser.Towards
 	Segement
 	Phase
@@ -36,13 +113,15 @@ Haibit.Andyuser.Towards
 */
 
 typedef wchar_t karact;
-typedef int integer;
+typedef unsigned short integer;
 
 karact
-ls53c2zl, ls8003zl,    ls606fzl, ls7f5112zl, ls67085zl, ls65e5zl, ls62a5zl, ls9053zl, ls636ezl,  ls8054zl, ls793ezl, ls7f8ezl,
-ls1zl, ls2zl,    ls3fzl, ls4zl, ls5zl, ls6zl, ls7zl, ls8zl, ls9ezl,  ls10zl, ls11ezl, ls12ezl;
-
+	ls53c2zl, ls8003zl,    ls606fzl, ls7f5112zl, ls67085zl, ls65e5zl, ls62a5zl, ls9053zl, ls636ezl,  ls8054zl, ls793ezl, ls7f8ezl,
+	ls1zl, ls2zl,    ls3fzl, ls4zl, ls5zl, ls6zl, ls7zl, ls8zl, ls9ezl,  ls10zl, ls11ezl, ls12ezl,
+	my,name, is,jacky,kma_asc_83,
+	your,age, is_syx,thrteen, dot_asc121;
 integer
-ls53c2el, ls8003el,    ls606fel, ls7f5112el, ls67085el, ls65e5el, ls62a5el, ls9053el, ls636eel,  ls8054el, ls793eel, ls7f8eel,
-ls1el, ls2el,    ls3fel, ls4el, ls5el, ls6el, ls7el, ls8el, ls9eel,  ls10el, ls11eel, ls12el;
-
+	ls53c2el, ls8003el,    ls606fel, ls7f5112el, ls67085el, ls65e5el, ls62a5el, ls9053el, ls636eel,  ls8054el, ls793eel, ls7f8eel,
+	ls1el, ls2el,    ls3fel, ls4el, ls5el, ls6el, ls7el, ls8el, ls9eel,  ls10el, ls11eel, ls12el;
+integer
+	dot_asc34,ace_asc32;
