6f97a11e5bac4bcd7cc874a0eada00745a61036e
SteelBall.Adventure <easyfound@126.com>
2021-04-12 10:05:24 +0800

    7 5 drag

--- Analytical/celly-gist/textarea.js
+++ Analytical/celly-gist/textarea.js
@@ -1,44 +1,41 @@
-﻿HTMLTextAreaElement.prototype.getCaretPosition = function () {
-    //return the caret position of the textarea
-    return this.selectionStart;
+﻿HTMLTextAreaElement.prototype.caret = function (start, end) {
+    switch(arguments.length) {
+        case 0:
+            return this.selectionStart;
+        case 1:
+            this.selectionStart = start;
+            this.selectionEnd = start;
+            this.focus();
+        case 2:
+            this.selectionStart = start;
+            this.selectionEnd = end;
+            this.focus();
+        return;
 };
-HTMLTextAreaElement.prototype.setCaretPosition = function (position) { //change the caret position of the textarea
-    this.selectionStart = position;
-    this.selectionEnd = position;
-    this.focus();
-};
-HTMLTextAreaElement.prototype.hasSelection = function () {
+HTMLTextAreaElement.prototype.careted = function () {
     //if the textarea has selection then return true
-    if (this.selectionStart == this.selectionEnd) {
-        return false;
-    } else {
-        return true;
-    }
+    return this.selectionStart == this.selectionEnd;
+    
 };
-HTMLTextAreaElement.prototype.getSelectedText = function () { //return the selection text
+HTMLTextAreaElement.prototype.kared = function () { //return the selection text
     return this.value.substring(this.selectionStart, this.selectionEnd);
-};
-HTMLTextAreaElement.prototype.setSelection = function (start, end) { //change the selection area of the textarea
-    this.selectionStart = start;
-    this.selectionEnd = end;
-    this.focus();
 };
 
 var textarea = document.getElementsByTagName("textarea")[0];
 
 textarea.onkeydown = function (event) {
-    var tabSpace = ;
+
     //support tab on textarea
     if (event.keyCode == 9) { //tab was pressed
         var newCaretPosition;
-        newCaretPosition = textarea.getCaretPosition() + tabSpace.length;
-        textarea.value = textarea.value.substring(0, textarea.getCaretPosition()) + tabSpace + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
-        textarea.setCaretPosition(newCaretPosition);
+        newCaretPosition = textarea.caret() + estb.length;
+        textarea.value = textarea.value.substring(0, textarea.caret()) + estb + textarea.value.substring(textarea.caret(), textarea.value.length);
+        textarea.caret(newCaretPosition);
         //tab key will blur this textarea
         return false;
     }
     if (event.keyCode == 13) {
-        var cursorPos = textarea.selectionStart;
+        var cursorPos = textarea.caret;
         var curentLine = textarea.value.substr(0, this.selectionStart).split("\n").pop();
         var indent = curentLine.match(/^\s*/)[0];
         //    curentLine = curentLine + tabSpace; 
